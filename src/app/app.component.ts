784df136ce4237db36b27d31791123cc5de00083
Decoder-Paul <paul93subhankar@gmail.com>
2019-06-17 12:41:09 +0530

    Setting up the project is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Udemy-App';
+  title = 'Angular-Udemy-App'; //Data binding
 }
