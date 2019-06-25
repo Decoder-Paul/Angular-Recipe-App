3abe087eee2aeb34736d32cbf3c968af23b32b8a
Subhankar Paul <paul93subhankar@gmail.com>
2019-06-25 20:58:22 +0530

    Till lecture 28|Completion:5%

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core'; // to let angular know about the 'Component' decorator
+
 @Component({  // to tells angular it's not a normal typescript class, it allows to enhance our class
   selector: 'app-server', // meta data for this class which will tell what to do with this class
   templateUrl: './server.component.html'
 })
-export class ServerComponent {
 
+export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+  getServerStatus(){
+    return this.serverStatus;
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-//  selector: 'app-servers',
-//  selector: '[app-servers]',
-  selector: '.app-servers', //but for id it can't be used
-//  templateUrl: './servers.component.html',
-  template: `
-    <app-server></app-server>`,
+  selector: 'app-servers',
+//  selector: '[app-servers]', //as a attribute of an element
+//  selector: '.app-servers', // but for id it can't be used
+  templateUrl: './servers.component.html',
+//  template: ,
   styleUrls: ['./servers.component.css']
 })
+
 export class ServersComponent implements OnInit {
-
-  constructor() { }
-
+  allowNewServer = false;
+  serverCreationStatus = 'No Server was created!';
+  constructor() {
+    setTimeout(() => { this.allowNewServer = true; }, 2000);
+  }
   ngOnInit() {
   }
-
+  onCreateServer(){
+    this.serverCreationStatus = 'Server is created';
+  }
 }
